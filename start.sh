b9d9c28f805c641876f29ce96f3e23506c820389
EletrixTime <71174682+eletrixtime@users.noreply.github.com>
2025-07-31 21:12:01 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,11 @@ GREEN="\033[0;32m"
 YELLOW="\033[1;33m"
 RED="\033[0;31m"
 RESET="\033[0m"
-
+echo -e "
+▄▖▖ ▄▖▄▖▄▖▄▖▖▖▄▖▖ ▄▖▖▖▄ 
+▙▖▌ ▙▖▐ ▙▘▐ ▚▘▌ ▌ ▌▌▌▌▌▌
+▙▖▙▖▙▖▐ ▌▌▟▖▌▌▙▖▙▖▙▌▙▌▙▘
+"
 # Function to print messages with colors
 log_success() {
     echo -e "${GREEN}[SUCCESS] $1${RESET}"
@@ -21,6 +25,7 @@ log_error() {
 
 # Clean up temp directory
 echo "⏳ Cleaning up temporary files..."
+mkdir -p logs
 if rm -rf /home/container/tmp/*; then
     log_success "Temporary files removed successfully."
 else
